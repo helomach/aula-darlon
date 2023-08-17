85e63249b1c374e08f76035cfa2826850fe96629
Heloisa Machado <106851899+helomach@users.noreply.github.com>
2023-08-16 21:10:48 -0300

    Delete container-py-streamlit directory

--- container-py-streamlit/hello.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-st.title("Hello from docker container!")
-with st.form(key='my form'):
-    submitted = st.form_submit_button('Click me')
-    if submitted:
-        st.balloons()
